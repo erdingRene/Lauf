daf0bdaa861c4cacc987c87fca630bec9e127738
HorstSeehofer <33832372+HorstSeehofer@users.noreply.github.com>
2017-12-01 16:19:20 +0100

    Adding SQLite DB Part 3 Preperation

--- app/src/main/java/de/simon/brandhuber/lauf/MainActivity.java
+++ app/src/main/java/de/simon/brandhuber/lauf/MainActivity.java
@@ -14,6 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     DatabaseHelper rundb;
+    EditText editRunNumber, editRunName;
+    Button btnOK;
+
     public static List<Location> weg = new ArrayList<Location>();
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -32,11 +38,15 @@ public class MainActivity extends AppCompatActivity
 
         //DB
         rundb = new DatabaseHelper(this);
-        //DB
+        editRunName = (EditText) findViewById(R.id.runNumber);
+        editRunName = (EditText) findViewById(R.id.runName);
+        btnOK = (Button) findViewById(R.id.btnOK);
+        AddData();
 
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +65,24 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    //Methode zum Hinzufügen der Eingaben zur DB per Schaltfläche
+    public void AddData() {
+        btnOK.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = rundb.insertData(Integer.parseInt(editRunName.getText().toString()),
+                                editRunName.getText().toString());
+                        if(isInserted = true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+        );
     }
 
     @Override
